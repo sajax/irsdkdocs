da890cba225eb825ac612e7af8ffaba79a1c8500
Simon Trendell <simon@trendell.net>
2025-04-25 14:56:58 +0100

    Switch to Vitepress

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vitepress'
+import { generateSidebar } from 'vitepress-sidebar';
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "iRacing SDK Documentation",
+  description: "Community documentation for iRacing SDK",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'YAML Structures', link: '/yaml/' },
+      { text: 'Telemetry', link: '/telemetry/' },
+      { text: 'iRacing.com', link: 'https://iracing.com' },
+    ],
+    sidebar: {
+      // fallback
+      '/yaml/': generateSidebar({
+        documentRootPath: 'docs',
+        scanStartPath: '/yaml',
+        useTitleFromFileHeading: true,
+        useFolderLinkFromIndexFile: true,
+        sortMenusByName: true
+      }),
+      '/telemetry/': generateSidebar({
+        documentRootPath: 'docs',
+        scanStartPath: '/telemetry',
+        useTitleFromFileHeading: true,
+        useFolderLinkFromIndexFile: true,
+        sortMenusByName: true
+      }),
+      '/': generateSidebar({
+        documentRootPath: 'docs',
+        scanStartPath: '/',
+        excludePattern: ['yaml/', 'telemetry/'],
+        useTitleFromFileHeading: true,
+        useFolderLinkFromIndexFile: true,
+        sortMenusByName: true
+      })
+    }
+  }
+})
